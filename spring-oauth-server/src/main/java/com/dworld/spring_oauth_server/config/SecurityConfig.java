cbfec49e0ca56975b51e96a566a8c2233d9eff27
Dwarika Prasad <dwarika.learning@gmail.com>
2025-01-13 20:29:38 +0530

    Integrated with postgres

--- spring-oauth-server/src/main/java/com/dworld/spring_oauth_server/config/SecurityConfig.java
+++ spring-oauth-server/src/main/java/com/dworld/spring_oauth_server/config/SecurityConfig.java
@@ -3,21 +3,27 @@ package com.dworld.spring_oauth_server.config;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import com.dworld.spring_oauth_server.repository.ClientRepository;
 
@@ -26,38 +32,49 @@ public class SecurityConfig {
 
     Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Bean
-    UserDetailsService inMemoryUserDetailsManager() {
-        log.info("Entered SecurityConfig");
-        // Below works
-        // return new InMemoryUserDetailsManager(
-        // User.withDefaultPasswordEncoder()
-        // .username("dwarika")
-        // .password("password")
-        // .roles("USER")
-        // .build()
-        // );
+    // @Bean
+    // UserDetailsService inMemoryUserDetailsManager() {
+    //     log.info("Entered SecurityConfig");
+    //     // Below works
+    //     // return new InMemoryUserDetailsManager(
+    //     // User.withDefaultPasswordEncoder()
+    //     // .username("dwarika")
+    //     // .password("password")
+    //     // .roles("USER")
+    //     // .build()
+    //     // );
 
-        // Below also works
-        UserBuilder userBuilder = User.builder();
-        UserDetails dwarika = userBuilder
-                .username("dwarika")
-                .password("{noop}password")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(dwarika);
+    //     // Below also works
+    //     UserBuilder userBuilder = User.builder();
+    //     UserDetails dwarika = userBuilder
+    //             .username("dwarika")
+    //             .password("{noop}password")
+    //             .roles("USER", "ADMIN")
+    //             .build();
+    //     return new InMemoryUserDetailsManager(dwarika);
+    // }
+
+    @Bean
+    JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+  
+    // @Bean
+    // RegisteredClientRepository registeredClientRepository() {
+    //     return new ClientRepository();
+    // }
+
+    @Bean
+    RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcRegisteredClientRepository(jdbcTemplate);
     }
 
     @Bean
-    RegisteredClientRepository registeredClientRepository() {
-        return new ClientRepository();
-    }
-
-    @Bean
-    ApplicationRunner clientsRunner(RegisteredClientRepository registeredClientRepository) {
+    ApplicationRunner clientsRunner(RegisteredClientRepository registeredClientRepository, UserDetailsManager userDetailsManager) {
         return args -> {
             var clientId = "demo-client";
-            registeredClientRepository.save(RegisteredClient
+            if(registeredClientRepository.findByClientId(clientId) == null) {
+                registeredClientRepository.save(RegisteredClient
                 .withId(UUID.randomUUID().toString())
                     .clientId(clientId)
                     .clientSecret("{noop}ourtopsecretsecret")
@@ -68,6 +85,16 @@ public class SecurityConfig {
                     .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build()
             );
+            }
+
+            if(!userDetailsManager.userExists("dwarika")) {
+                var userBuilder = User.builder();
+                UserDetails dwarika = userBuilder.username("dwarika")
+                    .password("{noop}password")
+                    .roles("USER", "ADMIN")
+                    .build();
+                    userDetailsManager.createUser(dwarika);
+            }
         };
     }
 }
